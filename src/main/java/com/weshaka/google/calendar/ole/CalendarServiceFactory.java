71297e7dec75ca21deb48fe960e8baa4c4b7f7f8
Ethan Ma <yixing.ma@gmail.com>
2015-08-27 21:51:06 -0700

    Google redirect_uri system in

--- src/main/java/com/weshaka/google/calendar/ole/CalendarServiceFactory.java
+++ src/main/java/com/weshaka/google/calendar/ole/CalendarServiceFactory.java
@@ -3,6 +3,7 @@
  */
 package com.weshaka.google.calendar.ole;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,10 +11,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -43,6 +44,12 @@ public class CalendarServiceFactory {
     /** Global instance of the scopes required by this quickstart. */
     private static final List<String> SCOPES = Arrays.asList(CalendarScopes.CALENDAR_READONLY);
 
+    private static final String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
+    
+    private static GoogleAuthorizationCodeFlow flow = null;
+    
+    static GoogleClientSecrets clientSecrets = loadClientSecrets();
+    
     static {
         try {
             HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
@@ -61,18 +68,68 @@ public class CalendarServiceFactory {
      */
     public static Credential authorize() throws IOException {
         // Load client secrets.
-        InputStream in = CalendarServiceFactory.class.getResourceAsStream("/client_secret.json");
-        System.out.println(in);
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        //InputStream in = CalendarServiceFactory.class.getResourceAsStream("/client_secret.json");
+        //System.out.println(in);
+        //GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
+        // Customize
+        String authorizeUrl = new GoogleAuthorizationCodeRequestUrl(clientSecrets, REDIRECT_URI, SCOPES).setState("").build();
+        System.out.println("Paste this URL into a web browser to authorize API Access:\n" + authorizeUrl);
+        System.out.println("... and type the code you received here: ");
+        BufferedReader in_authcode = new BufferedReader(new InputStreamReader(System.in));
+        String authorizationCode = in_authcode.readLine();
+        
+        Credential credential = exchangeCode(authorizationCode);
         // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(DATA_STORE_FACTORY).setAccessType("offline").build();
-        LocalServerReceiver localServerReceiver = new LocalServerReceiver.Builder().setPort(9089).build();//setHost("dev.weshaka.com").
-        System.out.println("Saving credentials...");
-        Credential credential = new AuthorizationCodeInstalledApp(flow, localServerReceiver).authorize("user");
-        System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+//        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+//                .setDataStoreFactory(DATA_STORE_FACTORY).setAccessType("offline").build();
+//        LocalServerReceiver localServerReceiver = new LocalServerReceiver.Builder().setPort(9089).build();//setHost("dev.weshaka.com").
+//        System.out.println("Saving credentials...");
+//        Credential credential = new AuthorizationCodeInstalledApp(flow, localServerReceiver).authorize("user");
+//        System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
         return credential;
+        
+       
+    }
+    
+    /**
+     * Helper to load client ID/Secret from file.
+     */
+    private static GoogleClientSecrets loadClientSecrets() {
+        try {
+            // Load client secrets.
+            InputStream in = CalendarServiceFactory.class.getResourceAsStream("/client_secret.json");
+            System.out.println(in);
+            GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+            return clientSecrets;
+        } catch (Exception e) {
+            System.out.println("Could not load clientsecrets.json");
+            e.printStackTrace();
+        }
+        return clientSecrets;
+    }
+    
+    /**
+     * Build an authorization flow and store it as a static class attribute.
+     */
+    static GoogleAuthorizationCodeFlow getFlow() {
+      if (flow == null) {
+        flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT,
+            JSON_FACTORY,
+            clientSecrets,
+            SCOPES)
+        .setAccessType("offline").setApprovalPrompt("force").build();
+      }
+      return flow;
+    }
+    /**
+     * Exchange the authorization code for OAuth 2.0 credentials.
+     */
+    static Credential exchangeCode(String authorizationCode) throws IOException  {
+      GoogleAuthorizationCodeFlow flow = getFlow();
+      GoogleTokenResponse response =
+          flow.newTokenRequest(authorizationCode).setRedirectUri(REDIRECT_URI).execute();
+      return flow.createAndStoreCredential(response, null);
     }
 
     /**
