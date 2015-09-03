5a64317c201465b04eee35caf493cd10bafcc3f6
Ethan Ma <yixing.ma@gmail.com>
2015-09-02 22:05:14 -0700

    automation script

---
+++ src/main/script/docker_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd $PWD/target
+echo $PWD
+sudo docker ps
+sudo docker rm -f $(sudo docker ps -aq -f "label=com.weshaka.label=ole_svc_1.0.0") 2>/dev/null || echo "No more containers to remove."
+sudo docker rmi -f $(sudo docker images -f "label=com.weshaka.label=ole_svc_1.0.0" -q) 2>/dev/null || echo "No more images to remove."
+sudo docker build -t shakahub/ole .
+sudo docker run -d -p 8080:8080 -p 9089:9089 --link shaka-mongo:mongo $(sudo docker images -f "label=com.weshaka.label=ole_svc_1.0.0" -q)
