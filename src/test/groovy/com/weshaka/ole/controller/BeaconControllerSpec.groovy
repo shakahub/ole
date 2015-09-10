ed8e7d3d81d15fe694c202de002bfc64e9d3c6ab
Ethan Ma <yixing.ma@gmail.com>
2015-09-10 10:41:59 -0700

    More spock testing for the beacon interface search by mac ID

--- src/test/groovy/com/weshaka/ole/controller/BeaconControllerSpec.groovy
+++ src/test/groovy/com/weshaka/ole/controller/BeaconControllerSpec.groovy
@@ -18,6 +18,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 
 import spock.lang.Specification
@@ -82,5 +83,25 @@ class BeaconControllerSpec extends Specification {
         BeaconSubject beaconSubject = entity.getBody()
         entity.statusCode == HttpStatus.OK
         beaconSubject.getBeacon().getMac()=="C1:5C:A0:2A:EC:F0"
+    }
+
+    @UsingDataSet(locations = ["/Beacon_subject_1.json"], loadStrategy = LoadStrategyEnum.INSERT)
+    void "Should return 404 from /beacons/C1:5C:A0:2A:EC:FF!"() {
+        when:
+        ResponseEntity entity = new RestTemplate().getForEntity("http://localhost:8090/beacons/C1:5C:A0:2A:EC:FF", BeaconSubject.class)
+
+        then:
+        final HttpClientErrorException exception = thrown()
+        exception.statusCode==HttpStatus.NOT_FOUND
+    }
+
+    @UsingDataSet(locations = ["/Beacon_subject_1.json"], loadStrategy = LoadStrategyEnum.INSERT)
+    void "Should return 400 from /beacons/C1:5C:A0:2A:EC!"() {
+        when:
+        ResponseEntity entity = new RestTemplate().getForEntity("http://localhost:8090/beacons/C1:5C:A0:2A:EC", BeaconSubject.class)
+
+        then:
+        final HttpClientErrorException exception = thrown()
+        exception.statusCode==HttpStatus.BAD_REQUEST
     }
 }
