cb3d8408263dd6aa899c3ec991bb7ad86346ac62
Ethan Ma <yixing.ma@gmail.com>
2015-09-09 21:13:39 -0700

    Calendar events integration testing

--- src/test/groovy/com/weshaka/ole/controller/BeaconControllerSpec.groovy
+++ src/test/groovy/com/weshaka/ole/controller/BeaconControllerSpec.groovy
@@ -1,11 +1,33 @@
 package com.weshaka.ole.controller;
 
-import spock.lang.Specification
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.boot.test.WebIntegrationTest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.web.client.RestTemplate
 
+import spock.lang.Specification
+import spock.lang.Stepwise
+
+import com.weshaka.ole.OleSvcApplication
+
+@ActiveProfiles("prod")
+@ContextConfiguration(loader = SpringApplicationContextLoader.class, classes = OleSvcApplication.class)
+@WebIntegrationTest
+@Stepwise
 class BeaconControllerSpec extends Specification {
-    void "testing spock works"(){
+
+    void "Testing spock works!"(){
         expect:
-        System.out.println("SPOCKTESTING...");
         true
+    }
+    void "Should return 200 from /calendar-envents!"() {
+        when:
+        ResponseEntity entity = new RestTemplate().getForEntity("http://localhost:8080/calendar-events", String.class)
+
+        then:
+        entity.statusCode == HttpStatus.OK
     }
 }
